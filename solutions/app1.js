2b9eb0dfa28e5643167d36717e3cc5d4e0b9f4eb
AnHuszti <husztiandrea82@gmail.com>
2021-11-18 22:07:08 +0100

    finished

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,7 +1,13 @@
+'use strict';
 
+function converterPromise(array) {
+    const converter = new Promise((resolve, reject) => {
+        if (array.every(el => typeof el === "string")) {    
+            resolve(array.map(el => el.toUpperCase()));
+        } else {
+            reject('Error: Not all elements are string type!');
+        }
+    }); return converter
+};
 
-
-
-
-
-// export default converterPromise;
+export default converterPromise;
